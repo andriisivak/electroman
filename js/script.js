2f296acf0ef986b8585237bf930333f039ef7872
rodrian <andrii.sivak@gmail.com>
2018-01-14 22:28:29 +0200

    em animation update

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-var game = new Phaser.Game(624, 384, Phaser.AUTO, '', {
+var game = new Phaser.Game(624, 384, Phaser.AUTO, '', { // 48px * 13, 48px * 8
   preload: preload,
   create: create,
   update: update
@@ -28,33 +28,33 @@ function create() {
   map.addTilesetImage('PLAN3');
   map.addTilesetImage('PLAN4');
 
-  // main background layer  
+  // main background layer
   layerBg = map.createLayer(0);
 
-  // objects layer  
+  // objects layer
   layerObjects = map.createLayer(1);
-  
+
   // monsters layer
   layerMonsters = map.createLayer(2);
-  
+
   // walls layer
   layerWalls = map.createLayer(3);
   game.physics.arcade.enable(layerWalls);
   layerWalls.resizeWorld();
-  
+
   game.physics.arcade.gravity.y = 700;
-  
+
   // electroman
   em = game.add.sprite(144, 260, 'em');
   game.physics.arcade.enable(em);
   em.body.setSize(28, 84, 10, 0); // set negative x-fields for em
 
-  em.animations.add('left', [17, 18, 19], 10, true);
-  em.animations.add('right', [7, 8, 9], 10, true);
-  
+  em.animations.add('left', [17, 18, 19, 20, 21, 22, 23, 24, 25, 26], 16, true);
+  em.animations.add('right', [7, 8, 9, 10, 11, 12, 13, 14, 15, 16], 16, true);
+
   game.physics.arcade.enable(em);
   game.camera.follow(em, Phaser.Camera.FOLLOW_TOPDOWN);
-  
+
   map.setCollisionBetween(0, 10000, true, layerWalls, true);
 
   cursors = game.input.keyboard.createCursorKeys();
@@ -64,7 +64,7 @@ function update() {
   game.physics.arcade.collide(em, layerWalls);
 
   em.body.velocity.x = 0;
-  
+
   if (cursors.left.isDown) {
     em.body.velocity.x = -150;
     if (facing != 'left') {
@@ -90,16 +90,16 @@ function update() {
       facing = 'idle';
     }
   }
-  
+
   if (cursors.up.isDown && em.body.onFloor() && game.time.now > jumpTimer) {
     em.body.velocity.y = -370;
     jumpTimer = game.time.now + 750;
   }
-  
+
   if (!em.body.onFloor() && facing === 'left') {
     em.frame = 19;
   }
-  
+
   if (!em.body.onFloor() && facing === 'right') {
     em.frame = 9;
   }
